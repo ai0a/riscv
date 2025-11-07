b729ad9f24bbf4cd7e5c76b5e610f0396f66cb58
zoe <git@z0e.me>
2025-11-07 15:52:43 -0600

    Fix decoding of B-type instructions

--- Sources/emulator/instruction decoding types/BType.swift
+++ Sources/emulator/instruction decoding types/BType.swift
@@ -17,11 +17,11 @@ struct BType {
         sourceRegister1 = RiscVDecoding.sourceRegister1(from: encodedInstruction)
         sourceRegister2 = RiscVDecoding.sourceRegister2(from: encodedInstruction)
         funct3 = RiscVDecoding.funct3(from: encodedInstruction)
-        let bits1thru4 = (encodedInstruction >> 8) & 0xf
+        let bits1thru4 = ((encodedInstruction >> 1) & 0x1e)
         let bit11 = ((encodedInstruction >> 7) & 1) << 11
         let bits5thru10 = (encodedInstruction & 0x7E000000) >> 20
-        let bit12 = encodedInstruction >> 30
+        let bit12 = (encodedInstruction >> 19) & 0x1000
         let uOffset = bits1thru4 | bits5thru10 | bit11 | bit12
-        offset = Int(uOffset.signExtension()) * 2
+        offset = Int(Int64(bitPattern: UInt64(uOffset).signExtension(ofBitCount: 13)))
     }
 }
