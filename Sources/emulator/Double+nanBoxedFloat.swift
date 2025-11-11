249f1b3b72e292ba0fdbb5cbc9cd4fabb40e4791
zoe <git@z0e.me>
2025-11-10 18:56:48 -0600

    Properly check for nan boxing in single precision floating point operations

--- Sources/emulator/Double+nanBoxedFloat.swift
+++ Sources/emulator/Double+nanBoxedFloat.swift
@@ -1,5 +1,8 @@
 extension Double {
 	var nanBoxedFloat: Float {
+		guard self.bitPattern & (0xffffffff << 32) == (0xffffffff << 32) else {
+			return .nan
+		}
 		let bitPattern = UInt32(self.bitPattern & 0xffffffff)
 		return Float(bitPattern: bitPattern)
 	}
